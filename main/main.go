ecbfdee5b3a4d734844b71a373f39a3427e48f4a
Amit Mukherjee <Sujata@Amits-MacBook-Air.local>
2016-04-30 12:11:16 -0500

    Renamed folder

--- main/main.go
+++ main/main.go
@@ -9,7 +9,7 @@ func main() {
 	//fmt.Printf("%d \t %b \t %#X \n",42,42,42)
 
 	for i := 0; i <= 200; i++ {
-		fmt.Printf("%d \t %b \t %x \t %q     \n", i, i, i, i)
+		fmt.Printf("%d \t %b \t %x \t %q     \n", i, i,Z i, i)
 	}
 
 }
