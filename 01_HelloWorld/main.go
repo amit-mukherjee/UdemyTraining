6f3b3dacd995fa0d0dc991d7853bd312135c6833
Amit Mukherjee <Sujata@Amits-MacBook-Air.local>
2016-04-30 02:44:48 -0500

    add changes to 01_HelloWorld Getting started

--- 01_HelloWorld/main.go
+++ 01_HelloWorld/main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!!")
+	//fmt.Println(42)
+	//fmt.Printf("%d - %b - %x\n",24,24,24)
+	//fmt.Printf("%d \t %b \t %#X \n",42,42,42)
+
+	for i := 0; i <= 200; i++ {
+		fmt.Printf("%d \t %b \t %x \t %q     \n", i, i, i, i)
+	}
+
 }
